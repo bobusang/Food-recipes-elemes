14d807ad6b78e5750073919b507a036317b4a820
Hamzah Ramadhan <hamzahram10@gmail.com>
2021-10-29 22:58:56 +0700

    installing tailwind

---
+++ src/Homepage/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Good Mood Food Us</h1>
+            <h2>Good Mood</h2>
+            <img src="https://imgur.com/cj1sIfC.png" alt="mobile-plate" />
+        </div>
+    )
+}
+
+export default Home

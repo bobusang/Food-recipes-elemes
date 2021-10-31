b25a106d2d6296d80266fdb0701ea74fdf4dc791
Hamzah Ramadhan <hamzahram10@gmail.com>
2021-10-31 19:48:22 +0700

    fixing bug

--- src/Homepage/components/Category.jsx
+++ src/Homepage/components/Category.jsx
@@ -26,7 +26,7 @@ function Category(props) {
         }
     }
     return (
-        <div className={`z-auto transition duration-500 ease-in-out hover:shadow-xl h-44 w-60 transform hover:translate-y-6  bg-red-300  hover:bg-catHover-pattern rounded-lg flex items-center justify-center`}>
+        <div className={`z-auto transition duration-500 ease-in-out hover:shadow-xl h-44 w-60 transform hover:translate-y-6  ${props.bg}  hover:bg-catHover-pattern rounded-lg flex items-center justify-center`}>
             <div className="text-center  px-20 py-7 font-semibold ">
                 {iconSet()}
                 <div className="mt-4 px-2 font-semibold text-base text-black">{props.title}</div>
